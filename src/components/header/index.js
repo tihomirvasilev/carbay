42e2a57bf7606e4cb9aa8ecb80163d43af1fb8c5
Tihomir Vasilev <rinzz86@yahoo.com>
2020-07-27 23:56:08 +0300

    fix: header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,59 @@
 import React from "react";
 
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {
+  BsBookmarksFill,
+  BsFillGrid3X3GapFill,
+  BsChevronExpand,
+  BsSearch,
+} from "react-icons/bs";
+
+import styles from "./index.module.css";
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="/">Carbuy</Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className={styles.navbar}
+      variant="dark"
+    >
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse
         id="responsive-navbar-nav"
-        className="justify-content-end text-center"
+        className="justify-content-between text-center"
       >
-        <Nav className="ml-auto">
-          <Nav.Link href="#features">Notebook</Nav.Link>
-          <Nav.Link href="#pricing">Search</Nav.Link>
+        <Nav>
+          <NavDropdown title={<BsFillGrid3X3GapFill />}>
+            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+            <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/create-ad">New Ad</NavDropdown.Item>
+            <NavDropdown.Item href="/my-ads">My Ads</NavDropdown.Item>
+            <NavDropdown.Item href="/notebook">My Notebook</NavDropdown.Item>
+            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/about">About Us</NavDropdown.Item>
+          </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/register">Register</Nav.Link>
+          <Nav.Link href="/notebook">
+            <BsBookmarksFill className={styles["icon-left"]} />
+            Notebook
+          </Nav.Link>
+        </Nav>
+        <Navbar.Brand href="/">CarBuy</Navbar.Brand>
+        <Nav>
+          <Nav.Link href="/sort">
+            <BsChevronExpand className={styles["icon-left"]} />
+            Sort
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/search">
+            <BsSearch className={styles["icon-left"]} />
+            Search
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
