e9442b3cab670ba3c4b15cf95d50ad2f67554e94
Tihomir Vasilev <rinzz86@yahoo.com>
2020-07-28 13:21:54 +0300

    feat: add register page

--- src/navigation.js
+++ src/navigation.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/login" exact component={LoginPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Header from "../../components/header";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div>
       <Header />
@@ -12,4 +12,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default HomePage;
