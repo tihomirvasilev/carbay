b199902d7d0be2e3ac939a41d0bb5163b0c212e1
Tihomir Vasilev <rinzz86@yahoo.com>
2020-08-12 01:34:59 +0300

    feat: connect to firebase storage, implement file upload and preview

--- src/components/model-form/index.js
+++ src/components/model-form/index.js
@@ -6,7 +6,7 @@ import FormValidation from "../../utils/from-validation";
 import ValidateModel from "./validation";
 
 const INITIAL_STATE = {
-  brandId: "",
+  brand: "",
   name: "",
 };
 
@@ -16,131 +16,82 @@ const ModelForm = () => {
   const { handleSubmit, handleChange, values } = FormValidation(
     INITIAL_STATE,
     ValidateModel,
-    handleAddModel
+    AddModel
   );
 
   const [brands, setBrands] = useState([]);
-  const [currentBrand, setCurrentBrand] = useState({});
-  const [currentModels, setCurrentModels] = useState(null);
+  const [brandId, setBrandId] = useState("");
+  const [models, setModels] = useState([]);
 
   useEffect(() => {
+    function getBrandsList() {
+      return firebase.db.collection("brands").onSnapshot(handleSnapshot);
+    }
+
+    async function handleSnapshot(snapshot) {
+      const brands = await snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setBrands(brands);
+    }
+
     getBrandsList();
-  });
+  }, [firebase]);
 
-  function getBrandsList() {
-    return firebase.db.collection("brands").onSnapshot(handleSnapshot);
-  }
-
-  async function handleSnapshot(snapshot) {
-    const brands = await snapshot.docs.map((doc) => {
-      return { id: doc.id, ...doc.data() };
-    });
-    setBrands(brands);
-  }
-
-  async function handleAddModel() {
-    const brandRef = firebase.db.collection("brands").doc(values.brandId);
+  async function AddModel() {
+    const brandId = await JSON.parse(values.brand).id;
+    const brandRef = firebase.db.collection("brands").doc(brandId);
 
     await brandRef.update({
       models: fire.firestore.FieldValue.arrayUnion(values.name),
     });
   }
 
-  function handleChangeOption(event) {
-    handleChange(event);
-    setCurrentBrand({
-      id: event.target.value,
-    });
-    getCurrentBrand();
-    //TODO: fix models list to get current models
-    console.log(event.target);
-  }
-
-  function getCurrentBrand() {
-    const brand = brands.filter((x) => x.id === currentBrand.id)[0];
-    console.log(brand);
-    if (brand) {
-      setCurrentModels(brand.models);
-    }
-  }
-
-  if (currentModels) {
-    return (
-      <>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="exampleForm.ControlSelect1">
-            <Form.Control
-              as="select"
-              name="brandId"
-              onChange={handleChangeOption}
-              value={values.brandId}
-            >
-              <option value="">SELECT BRAND</option>
-              {brands.map((b) => (
-                <option key={b.id} value={b.id}>
-                  {b.name}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control
-              onChange={handleChange}
-              name="name"
-              type="text"
-              placeholder="Model Name"
-              value={values.name}
-            />
-          </Form.Group>
-          <Form.Group hidden>
-            <Form.Control
-              onChange={handleChange}
-              name="models"
-              type="text"
-              placeholder="Model Name"
-              value={values.models}
-            />
-          </Form.Group>
-          <Button type="submit">Add</Button>
-        </Form>
-        <br />
-        <ul>
-          {currentModels.map((model, key) => (
-            <li key={key}>{model}</li>
-          ))}
-        </ul>
-      </>
-    );
+  function handleChangeOption(e) {
+    const currentBrand = JSON.parse(e.target.value);
+    setBrandId(currentBrand.id);
+    setModels(currentBrand.models);
+    handleChange(e);
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="exampleForm.ControlSelect1">
-        <Form.Control
-          as="select"
-          name="brandId"
-          onChange={handleChangeOption}
-          value={values.brandId}
-        >
-          <option value="">SELECT BRAND</option>
-          {brands.map((b) => (
-            <option key={b.id} value={b.id}>
-              {b.name}
-            </option>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Control
+            as="select"
+            name="brand"
+            onChange={handleChangeOption}
+            value={brandId}
+          >
+            <option value="">SELECT BRAND</option>
+            {brands.map((b) => (
+              <option key={b.id} value={JSON.stringify(b)}>
+                {b.name}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            onChange={handleChange}
+            name="name"
+            type="text"
+            placeholder="Model Name"
+            value={values.name}
+          />
+        </Form.Group>
+        <Button type="submit">Add</Button>
+      </Form>
+      <br />
+      {models.length > 0 && (
+        <ul>
+          {models.map((model, index) => (
+            <li key={index}>{model}</li>
           ))}
-        </Form.Control>
-      </Form.Group>
-      <Form.Group>
-        <Form.Control
-          onChange={handleChange}
-          name="name"
-          type="text"
-          placeholder="Model Name"
-          value={values.name}
-        />
-      </Form.Group>
-      <Button type="submit">Add</Button>
-    </Form>
+        </ul>
+      )}
+    </>
   );
 };
 
