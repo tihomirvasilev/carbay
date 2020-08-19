ea8a256387e07b56e9c6c4ebce7f5aab74188b71
Tihomir Vasilev <rinzz86@yahoo.com>
2020-08-20 00:06:06 +0300

    fix: file missing

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+const GC = {
+  ROUTES: {
+    USER: {
+      HOME: "/",
+      LOGIN: "/login",
+      REGISTER: "/register",
+      MYADS: "/my-ads",
+      NEWAD: "/new-ad",
+      AD: "/ad/:id",
+      FAVORITES: "/favorites",
+      EDITAD: "/edit/:id",
+    },
+    ADMIN: {
+      PANEL: "/admin",
+      BRANDS: "/admin/brands",
+      MODELS: "/admin/models",
+      OPTIONS: "/admin/options",
+      ADS: "/admin/ads",
+    },
+  },
+};
+
+export default GC;
